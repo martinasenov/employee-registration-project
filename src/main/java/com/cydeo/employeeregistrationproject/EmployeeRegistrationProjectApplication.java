385326ae33e209218b796edbbcec587781508ef3
martinasenov <martin.manuilov.asenov@gmail.com>
2024-04-07 21:04:20 +0200

    Employee Registration Form HTML part completed

---
+++ src/main/java/com/cydeo/employeeregistrationproject/EmployeeRegistrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.cydeo")
+public class EmployeeRegistrationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistrationProjectApplication.class, args);
+    }
+
+}
